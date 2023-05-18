1493cc976cc8468ac079da0d0f439a31c9f78748
Tareq Sifat <mdtareq.me@gmail.com>
2023-05-18 12:49:11 +0600

    login done

--- Gemfile
+++ Gemfile
@@ -77,3 +77,5 @@ group :test do
   gem "selenium-webdriver"
   gem "webdrivers"
 end
+
+gem "jwt", "~> 2.7"
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 
+require 'jwt'
 class ApplicationController < ActionController::Base
     protect_from_forgery with: :null_session
 end
