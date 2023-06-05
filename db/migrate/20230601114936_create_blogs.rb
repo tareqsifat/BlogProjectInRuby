8319ef47e484fc72fd232cf3c2c5d2138ce25b30
Tareq Sifat <mdtareq.me@gmail.com>
2023-06-05 14:58:46 +0600

    model migration relaiton done for blgs

--- db/migrate/20230601114936_create_blogs.rb
+++ db/migrate/20230601114936_create_blogs.rb
@@ -1,13 +1,13 @@
 class CreateBlogs < ActiveRecord::Migration[7.0]
   def change
     create_table :blogs do |t|
-      t.string :user_id, limit:10, default:'NULL',
-      t.string :category_id, limit:10, default:'NULL',
-      t.string :sub_category_id, limit:10, default:'NULL',
-      t.string :title, limit:100, default:'NULL',
-      t.string :slug, limit:100, default:'NULL',
-      t.string :body, default:'NULL',
-      t.string :image, default:'NULL',
+      t.string :user_id, limit:10, default:'NULL'
+      t.string :category_id, limit:10, default:'NULL'
+      t.string :sub_category_id, limit:10, default:'NULL'
+      t.string :title, limit:100, default:'NULL'
+      t.string :slug, limit:100, default:'NULL'
+      t.string :body, default:'NULL'
+      t.string :image, default:'NULL'
       t.timestamps
     end
 
