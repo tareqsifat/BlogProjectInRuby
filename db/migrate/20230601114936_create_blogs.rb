dd43933ed9853e56bea489162f25c3f1afc58f2a
Tareq Sifat <mdtareq.me@gmail.com>
2023-06-05 13:54:19 +0600

    blog migration done

--- db/migrate/20230601114936_create_blogs.rb
+++ db/migrate/20230601114936_create_blogs.rb
@@ -1,12 +1,16 @@
 class CreateBlogs < ActiveRecord::Migration[7.0]
   def change
     create_table :blogs do |t|
-      t.string :user_id, limit:10, default:'NULL', null:true,
-      t.string :title, limit:100, default:'NULL', null:true,
-      t.string :slug, limit:100, default:'NULL', null:true,
-      t.string :body, default:'NULL', null:true,
-      t.string :image, default:'NULL', null:true,
+      t.string :user_id, limit:10, default:'NULL',
+      t.string :category_id, limit:10, default:'NULL',
+      t.string :sub_category_id, limit:10, default:'NULL',
+      t.string :title, limit:100, default:'NULL',
+      t.string :slug, limit:100, default:'NULL',
+      t.string :body, default:'NULL',
+      t.string :image, default:'NULL',
       t.timestamps
     end
+
+    add_index :blogs, :slug
   end
 end
