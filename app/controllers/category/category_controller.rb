60f8decb6fcd9dd525adde899cabd8884b3d30e9
Tareq Sifat <mdtareq.me@gmail.com>
2023-06-05 18:04:18 +0600

    working on category save

--- app/controllers/category/category_controller.rb
+++ app/controllers/category/category_controller.rb
@@ -1,14 +1,29 @@
 class Category::CategoryController < ApplicationController
-
+    skip_before_action :verify_authenticity_token
     def index
         categories = Category.all
         if categories.length != 0 then
-            render json: ["data"=>categories], status: :ok
+            render json: categories, status: :ok
         else
             render json: ["Message"=>"there is no category"], status: :ok
         end
     end
     
+    def create
+        category = Category.new(category_params)
+        if category.save
+            render json: { message: "Category created successfully." }, status: :created
+          else
+            render json: { errors: category.errors.full_messages }, status: :bad_request
+          end
+        
+    end
+
+
+    def category_params
+        params.require(:category).permit(:name, :image)
+      end
+
 
 
 end
--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,4 +1,6 @@
 class Blog < ApplicationRecord
+    validates :title, presence: true, length: { minimum:5, maximum:40 }
+    validates :body, presence: true, length: { minimum:10, maximum:1500 }
     belongs_to :user
 
     belongs_to :categories, class_name: "category"
